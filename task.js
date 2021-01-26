5b6c48cb55e945605d9e2ab8d387122ab4b12e79
Tatiana Chueva <tatianachueva@Tatianas-MacBook-Pro.local>
2021-01-26 09:42:22 -0800

    add min

--- task.js
+++ task.js
@@ -1,11 +1,10 @@
 function victotySwim(arr){
-    let res = '';
     let min = arr[0][1];
     for (let i = 1; i < arr.length; i++){
         if (arr[i][1] < min){
-            res = arr[i][1];
+            min = arr[i][1];
         }
     }
-    return +res;
+    return min;
 }
 console.log(victotySwim([['Nick', 0], ['Bob', 10], ['Kim', 5], ['Kim', 6]])); //6
